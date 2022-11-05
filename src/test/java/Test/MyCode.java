a0128907b3ab9e3c015aac2b1d750b91101ae28f
moinm <moinm@LAPTOP-QIG6G3JF>
2022-11-05 22:39:30 +0530

    My commit 3

---
+++ src/test/java/Test/MyCode.java
@@ -0,0 +1,20 @@
+package Test;
+
+public class MyCode {
+
+	public static void main(String[] args) {
+		
+		int a=10;
+		int b=20;
+		
+		int c=(a+b);
+		System.out.println("addition of a&b :"+c);
+		
+		
+		
+		
+		
+		
+	}
+
+}
